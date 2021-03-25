35fdbef8298315350e09e5a0d929fb727df08af0
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-03-25 19:06:28 -0400

    feat: add 11ty plugin to add `target="_blank" rel="noopener"` for all external links

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
+const externalLinks = require('eleventy-plugin-external-links') // https://github.com/vimtor/eleventy-plugin-external-links
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(externalLinks, {
+    // Plugin defaults:
+    name: 'external-links',         // Plugin name
+    regex: /^(([a-z]+:)|(\/\/))/i,  // Regex that test if href is external
+    target: "_blank",               // 'target' attribute for external links
+    rel: "noopener",                // 'rel' attribute for external links
+    extensions: [".html"],          // Extensions to apply transform to
+  })
+
   return {
     dir: {
       input: 'src',
